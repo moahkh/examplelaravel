b95b28bc639bb94c16f7888550f7e60a1f2cb872
moahkh <mo_ah_kh@hotmail.com>
2023-09-26 13:17:06 +0300

    secound update

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/n', function () {
-    return "kkkkkk";
+Route::get('/m', function () {
+    return "routeeesssm";
 });
